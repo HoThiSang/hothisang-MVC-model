91a3f0199363e4b2d3fb83c80ad3890d8fe024ce
sang.ho <sang.ho25@student.passerellesnumeriques.org>
2023-12-27 19:33:46 +0700

    add file

--- controller/post/post.update.controller.php
+++ controller/post/post.update.controller.php
@@ -1,12 +1,13 @@
 <?php
 require_once dirname(dirname(__DIR__)) . '/model/post.model.php';
-
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $id = $_GET['id'] ? $_GET['id'] : '';
     $title = $_POST['title'];
     $description = $_POST['description'];
     echo $title;
-    if (!empty($title)  && !empty($description)) {
+    if ( isset($id) && !empty($title)  && !empty($description)) {
         $post = updatePost($id, $title, $description);
-         header('Location: /start-code/post');
-        require dirname(dirname(__DIR__)) . "/views/post/post.view.php";
+        header('location: /start-code/post');
     }
+   require dirname(dirname(__DIR__)) . "/views/post/post.view.php";
+}
